8220d2368a64bf8bdecee9cfffedb4684fc4fbbc
D4l3k <rice@outerearth.net>
2012-01-25 18:17:45 -0800

    Checks for updates now.

--- launch.rb
+++ launch.rb
@@ -17,7 +17,22 @@ config = XmlSimple.xml_in(xml_data)
 #puts config.to_s
 latest_secondary_version = config["versioning"][0]["snapshotVersions"][0]["snapshotVersion"][0]["value"][0]
 puts "Latest CraftBukkit Minor Version: #{latest_secondary_version}"
-url = base_url+latest_primary_version+"/"+"craftbukkit-"+latest_secondary_version+".jar"
-puts "Download Link: "+url
-download_command = "rm bukkit_tmp.jar; time axel -a -o bukkit_tmp.jar #{url};mv bukkit_tmp.jar bukkit.jar;#{launch_command}"
-exec(download_command)
+version_file = ".bukkit_version"
+if File.exists?(version_file)
+	File.open(version_file, "r") do |object|
+		version = object.read
+	end
+end
+cur_version = latest_primary_version+";"+latest_secondary_version
+if version != cur_version
+	url = base_url+latest_primary_version+"/"+"craftbukkit-"+latest_secondary_version+".jar"
+	puts "New CraftBukkit version Found! \nDownload Link: "+url
+	download_command = "rm bukkit_tmp.jar; axel -a -o bukkit_tmp.jar #{url};mv bukkit_tmp.jar bukkit.jar;#{launch_command}"
+	File.open(version_file, "w") do |object|
+		object.print cur_version
+	end
+	exec(download_command)
+else
+	puts "CraftBukkit up to date."
+	exec(launch_command)
+end
